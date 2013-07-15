8eb2d126998134fcb95338f1942e15c3dfb16155
davidwalton3142 <davidwalton3142@gmail.com>
2013-07-15 18:05:06 +0100

    Finished SHMat - now test

--- src/SHMat.cpp
+++ src/SHMat.cpp
@@ -30,6 +30,40 @@ std::vector<float> SHMat::operator * (const std::vector<float>& p)
 	return ans;
 }
 
+void SHMat::init(const glm::mat3& rotation, int nBands)
+{
+	Matrix<float> R_o(rotation);
+	/* Rearrange coeffts of R */
+	Matrix<float> R(3,3);
+	R(0,0) = R_o(1,1);
+	R(0,1) = R_o(2,1);
+	R(0,2) = R_o(1,0);
+	R(1,0) = R_o(2,1);
+	R(1,1) = R_o(2,2);
+	R(1,2) = R_o(2,0);
+	R(2,0) = R_o(0,1);
+	R(2,1) = R_o(0,2);
+	R(2,2) = R_o(0,0);
+
+	for(int l = 0; l < nBands; ++l)
+	{
+		Matrix<float> mat(2*l + 1, 2*l + 1);
+		for(int m = -l; m <= l; ++m)
+			for(int n = -l; n <= l; ++n)
+			{
+				mat.i(m,n) = M(l,m,n,R);
+			}
+		blocks.push_back(mat);
+	}
+}
+
+float M(int l, int m, int n, const Matrix<float>& R)
+{
+	return u(l,m,n) * U(l,m,n,R)
+		 + v(l,m,n) * V(l,m,n,R)
+		 + w(l,m,n) * W(l,m,n,R);
+}
+
 float SHMat::P(int i, int l, int m, int n, const Matrix<float>& R)
 {
 	if(n == l)
@@ -72,12 +106,12 @@ float SHMat::w(int l, int m, int n)
 	return -0.5 * sqrt(num / den) * (1 - del(m,0));
 }
 
-float U(int l, int m, int n, const Matrix<float>& R)
+float SHMat::U(int l, int m, int n, const Matrix<float>& R)
 {
 	return P(0, l, m, n, R);
 }
 
-float V(int l, int m, int n, const Matrix<float>& R)
+float SHMat::V(int l, int m, int n, const Matrix<float>& R)
 {
 	if(m == 0)
 		return P(1, l, 1, n) * P(-1, l, -1, n);
@@ -87,7 +121,7 @@ float V(int l, int m, int n, const Matrix<float>& R)
 		return P(1, l, m+1, n) * (1 + del(m,-1)) + P(-1, 1, -m-1, n) * sqrt(1 - del(m,-1)); 
 }
 
-float W(int l, int m, int n, const Matrix<float>& R)
+float SHMat::W(int l, int m, int n, const Matrix<float>& R)
 {
 	/* Shouldn't be called with m == 0 */
 	if(m == 0)
